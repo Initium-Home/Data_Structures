551f758cd9df0673882f8759497292b23fc58747
master <laksj113@gmail.com>
2023-04-14 19:38:03 +0900

    Add data structures by 02

---
+++ _02_SinglyLinkedList/Node.java
@@ -0,0 +1,12 @@
+package _02_SinglyLinkedList;
+
+class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+}
